93f09da2f6160c30b77e227624105e15967fba22
PHANNI6556 <PHANNIPA.KH@cpird.in.th>
2025-09-10 15:59:45 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,7 +1,8 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwR5z1QhMd3588Hn5pW7kF55p0pUp4AG2ZmbzVDHTOhNBOFjjPuWdKIny8G8_rT0rodmA/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxu9h0ZJLm0h__BEahkGehV5nahKBs-aED6V4hYQnw-XxBmoUEuxIyZ4YHr_CRXEm53/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyN5SXimUgqcMrVY_4jjae68PPfD4Cp8lEQdDejMY1X9RP0VorCl4a7yF-sP5A3AUtQ7A/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwA9FwP0tGEXEzh0yEfXhUa94zLCTik3r6VVjuTOgoDwa_XLkjGHFyNpETghlSaipY_/exec';
 //LIFF_ID
-    const LIFF_ID = '2007747842-OGeyrGkv'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2007747842-p0YX1oBn'; // LIFF ID history
-    const LIFF_REGISTER = '2007747842-oJr1GXK2'; // LIFF ID register
+    const LIFF_ID = '2008087989-5rLlRYYn'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2008087989-j7WA566E'; // LIFF ID history
+    const LIFF_REGISTER = '2008087989-Le5oxNNr'; // LIFF ID register
+
