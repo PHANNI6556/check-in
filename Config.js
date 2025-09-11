47aa3b0206eb3aa2f06e1692d958d907e2f1aaa1
PHANNI6556 <PHANNIPA.KH@cpird.in.th>
2025-09-11 15:11:58 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,9 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyN5SXimUgqcMrVY_4jjae68PPfD4Cp8lEQdDejMY1X9RP0VorCl4a7yF-sP5A3AUtQ7A/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwXJDc60MBuh6f87BJM217sgBEzFX10WhOdAXlrVGxzsUrBr5uIoycSs3sCYitu6XESVA/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwA9FwP0tGEXEzh0yEfXhUa94zLCTik3r6VVjuTOgoDwa_XLkjGHFyNpETghlSaipY_/exec';
 //LIFF_ID
     const LIFF_ID = '2008087989-5rLlRYYn'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008087989-j7WA566E'; // LIFF ID history
     const LIFF_REGISTER = '2008087989-Le5oxNNr'; // LIFF ID register
+
 
